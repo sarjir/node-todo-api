56a58affb79a41c91829165555aac042f975178f
Sara Jirholm <sara.jirholm@hotmail.com>
2019-06-02 16:10:07 +0200

    Add first working example with express and mongo

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-app.get('/', (req, res) => res.send('Hello dddzzzzzsdsdz!'))
-app.get('/1', (req, res) => res.send('12345'))
-app.get('/2', (req, res) => res.send('Hello World! 22222dfdfdf33333'))
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+const dbUrl = 'mongodb://mongo:27017';
+const dbName = 'todo-api';
+const client = new MongoClient(dbUrl);
+
+app.post('/todos/add', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200);
+  res.json(req.body);
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
